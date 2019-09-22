f37ca923849ee4c8dfac5dcdaed10cf530a39252
katherineggs <katherinegarcia@ufm.edu>
2019-09-22 14:20:43 -0600

    adds top menu estudios No pretty

--- soup.py
+++ soup.py
@@ -2,12 +2,12 @@
 from bs4 import BeautifulSoup
 import requests,sys,csv,json
 
-url="http://ufm.edu/Portal"
+url_1="http://ufm.edu/Portal"
 # Make a GET request to fetch the raw HTML content
 try:
-    html_content = requests.get(url).text
+    html_content = requests.get(url_1).text
 except:
-    print(f"unable to get {url}")
+    print(f"unable to get {url_1}")
     sys.exit(1)
 
 # Parse the html content, this is the Magic ;)
@@ -24,8 +24,8 @@ title = soup.title.string
 print(title)
 print(item)
 
-image_href = []
 hrefs = soup.find_all("a", href = True)
+image_href = []
 for i in hrefs:
     ref = i['href']
 
@@ -46,9 +46,13 @@ for i in hrefs:
     elif i.text == "MiU":
         miubutton = ref
 
+    elif i.text == "Estudios":
+        estudios = ref
+
     imag = i.find_all("img")
     for im in imag:
         image_href.append(ref)
+    
 
 print("Direccion:", direccion)
 print("Telefono:", telefono)
@@ -63,19 +67,31 @@ print(mailbutton,"\n")
 print(miubutton, "\n")
 for i in image_href:
     print(i)
-        
-# contador = 1
-# prophref = soup.find_all("a", href = True)
-# for i in prophref:s
-#     ref = i['href']
-#     if not ref.startswith(("#", "/")):
-#         print(contador, ref)
-#         contador += 1
+contador=0
+for i in soup.find_all("a"):
+    contador += 1
+print("\nThere are",contador,"<a> tags\n")
 
-#ufmail = soup.find("meta", property="og:image").get('content')
 
 print("2. Estudios")
 print(parte)
+url_2 = "http://ufm.edu" + estudios
+# Make a GET request to fetch the raw HTML content
+try:
+    html_content = requests.get(url_2).text
+except:
+    print(f"unable to get {url_2}")
+    sys.exit(1)
+# Parse the html content, this is the Magic ;)
+soup = BeautifulSoup(html_content, "html.parser")
+
+top = soup.find("div", id="topmenu")
+print(top.text)
+# for ul in top:
+#     print(ul)
+
+
+
 
 
 
